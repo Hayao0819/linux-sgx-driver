855376e99c3c2815db01a68b104d7edfcecc7508
hayao <shun819.mail@gmail.com>
2025-07-14 15:14:52 +0900

    Fix compile error in the latest linux

--- sgx_main.c
+++ sgx_main.c
@@ -86,10 +86,10 @@ MODULE_DESCRIPTION(DRV_DESCRIPTION);
 MODULE_AUTHOR("Jarkko Sakkinen <jarkko.sakkinen@linux.intel.com>");
 MODULE_VERSION(DRV_VERSION);
 #ifndef X86_FEATURE_SGX
-	#define X86_FEATURE_SGX (9 * 32 + 2)
+#define X86_FEATURE_SGX (9 * 32 + 2)
 #endif
 #ifndef FEAT_CTL_SGX_ENABLED
-#define FEAT_CTL_SGX_ENABLED                      (1<<18)
+#define FEAT_CTL_SGX_ENABLED (1 << 18)
 #endif
 /*
  * Global data.
@@ -111,16 +111,16 @@ static int sgx_mmap(struct file *file, struct vm_area_struct *vma)
 {
 	vma->vm_ops = &sgx_vm_ops;
 	vm_flags_set(vma, VM_PFNMAP | VM_DONTEXPAND | VM_DONTDUMP | VM_IO |
-                    VM_DONTCOPY);
-	
+						  VM_DONTCOPY);
+
 	return 0;
 }
 
 static unsigned long sgx_get_unmapped_area(struct file *file,
-					   unsigned long addr,
-					   unsigned long len,
-					   unsigned long pgoff,
-					   unsigned long flags)
+										   unsigned long addr,
+										   unsigned long len,
+										   unsigned long pgoff,
+										   unsigned long flags)
 {
 	if (len < 2 * PAGE_SIZE || (len & (len - 1)) || flags & MAP_PRIVATE)
 		return -EINVAL;
@@ -142,8 +142,7 @@ static unsigned long sgx_get_unmapped_area(struct file *file,
 		return -EINVAL;
 #endif
 
-	addr = current->mm->get_unmapped_area(file, addr, 2 * len, pgoff,
-					      flags);
+	addr = get_unmapped_area(file, addr, 2 * len, pgoff, flags);
 	if (IS_ERR_VALUE(addr))
 		return addr;
 
@@ -153,27 +152,27 @@ static unsigned long sgx_get_unmapped_area(struct file *file,
 }
 
 static const struct file_operations sgx_fops = {
-	.owner			= THIS_MODULE,
-	.unlocked_ioctl		= sgx_ioctl,
+	.owner = THIS_MODULE,
+	.unlocked_ioctl = sgx_ioctl,
 #ifdef CONFIG_COMPAT
-	.compat_ioctl		= sgx_ioctl,
+	.compat_ioctl = sgx_ioctl,
 #endif
-	.mmap			= sgx_mmap,
-	.get_unmapped_area	= sgx_get_unmapped_area,
+	.mmap = sgx_mmap,
+	.get_unmapped_area = sgx_get_unmapped_area,
 };
 
 static struct miscdevice sgx_dev = {
- .minor	= MISC_DYNAMIC_MINOR,
-	.name	= "isgx",
-	.fops	= &sgx_fops,
-	.mode   = 0666,
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "isgx",
+	.fops = &sgx_fops,
+	.mode = 0666,
 };
 
 static struct miscdevice sgx_dev_alt = {
- .minor	= MISC_DYNAMIC_MINOR,
-	.name	= "sgx",
-	.fops	= &sgx_fops,
-	.mode   = 0666,
+	.minor = MISC_DYNAMIC_MINOR,
+	.name = "sgx",
+	.fops = &sgx_fops,
+	.mode = 0666,
 };
 
 static bool has_flc(void)
@@ -183,7 +182,8 @@ static bool has_flc(void)
 	return (ecx >> 30) == 0x1;
 }
 
-static struct miscdevice *sgx_device(void) {
+static struct miscdevice *sgx_device(void)
+{
 	if (sgx_dev_alt_name || has_flc())
 		return &sgx_dev_alt;
 	else
@@ -195,8 +195,10 @@ static int sgx_pm_suspend(struct device *dev)
 	struct sgx_tgid_ctx *ctx;
 	struct sgx_encl *encl;
 
-	list_for_each_entry(ctx, &sgx_tgid_ctx_list, list) {
-		list_for_each_entry(encl, &ctx->encl_list, encl_list) {
+	list_for_each_entry(ctx, &sgx_tgid_ctx_list, list)
+	{
+		list_for_each_entry(encl, &ctx->encl_list, encl_list)
+		{
 			sgx_invalidate(encl, false);
 			encl->flags |= SGX_ENCL_SUSPEND;
 			flush_work(&encl->add_page_work);
@@ -237,21 +239,24 @@ static int sgx_dev_init(struct device *parent)
 #endif
 	sgx_encl_size_max_32 = 1ULL << (edx & 0xFF);
 
-	if (boot_cpu_has(X86_FEATURE_OSXSAVE)) {
+	if (boot_cpu_has(X86_FEATURE_OSXSAVE))
+	{
 		cpuid_count(SGX_CPUID, SGX_CPUID_ATTRIBUTES, &eax, &ebx, &ecx,
-			    &edx);
+					&edx);
 		sgx_xfrm_mask = (((u64)edx) << 32) + (u64)ecx;
 
-		for (i = 2; i < 64; i++) {
+		for (i = 2; i < 64; i++)
+		{
 			cpuid_count(0x0D, i, &eax, &ebx, &ecx, &edx);
 			if ((1ULL << i) & sgx_xfrm_mask)
 				sgx_xsave_size_tbl[i] = eax + ebx;
 		}
 	}
 
-	for (i = 0; i < SGX_MAX_EPC_BANKS; i++) {
+	for (i = 0; i < SGX_MAX_EPC_BANKS; i++)
+	{
 		cpuid_count(SGX_CPUID, i + SGX_CPUID_EPC_BANKS, &eax, &ebx,
-			    &ecx, &edx);
+					&ecx, &edx);
 		if (!(eax & 0xf))
 			break;
 
@@ -266,20 +271,23 @@ static int sgx_dev_init(struct device *parent)
 
 	sgx_nr_epc_banks = i;
 
-	for (i = 0; i < sgx_nr_epc_banks; i++) {
+	for (i = 0; i < sgx_nr_epc_banks; i++)
+	{
 #ifdef CONFIG_X86_64
 		sgx_epc_banks[i].va = (unsigned long)
 			ioremap_cache(sgx_epc_banks[i].pa,
-				      sgx_epc_banks[i].size);
-		if (!sgx_epc_banks[i].va) {
+						  sgx_epc_banks[i].size);
+		if (!sgx_epc_banks[i].va)
+		{
 			sgx_nr_epc_banks = i;
 			ret = -ENOMEM;
 			goto out_iounmap;
 		}
 #endif
 		ret = sgx_add_epc_bank(sgx_epc_banks[i].pa,
-				       sgx_epc_banks[i].size, i);
-		if (ret) {
+							   sgx_epc_banks[i].size, i);
+		if (ret)
+		{
 			sgx_nr_epc_banks = i + 1;
 			goto out_iounmap;
 		}
@@ -290,8 +298,9 @@ static int sgx_dev_init(struct device *parent)
 		goto out_iounmap;
 
 	sgx_add_page_wq = alloc_workqueue("intel_sgx-add-page-wq",
-					  WQ_UNBOUND | WQ_FREEZABLE, 1);
-	if (!sgx_add_page_wq) {
+									  WQ_UNBOUND | WQ_FREEZABLE, 1);
+	if (!sgx_add_page_wq)
+	{
 		pr_err("intel_sgx: alloc_workqueue() failed\n");
 		ret = -ENOMEM;
 		goto out_page_cache;
@@ -299,13 +308,15 @@ static int sgx_dev_init(struct device *parent)
 
 	dev->parent = parent;
 	ret = misc_register(dev);
-	if (ret) {
+	if (ret)
+	{
 		pr_err("intel_sgx: misc_register() failed\n");
 		goto out_workqueue;
 	}
 
 	on_each_cpu(sgx_reset_pubkey_hash, &msr_reset_failed, 1);
-	if (msr_reset_failed) {
+	if (msr_reset_failed)
+	{
 		pr_info("intel_sgx:  can not reset SGX LE public key hash MSRs\n");
 	}
 
@@ -327,41 +338,46 @@ static int sgx_drv_probe(struct platform_device *pdev)
 {
 	unsigned int eax, ebx, ecx, edx;
 	unsigned long fc;
-	if (atomic_cmpxchg(&sgx_init_flag, 0, 1)) {
+	if (atomic_cmpxchg(&sgx_init_flag, 0, 1))
+	{
 		pr_warn("intel_sgx: second initialization call skipped\n");
 		return 0;
 	}
-
 
 	if (boot_cpu_data.x86_vendor != X86_VENDOR_INTEL)
 		return -ENODEV;
 
 	cpuid(7, &eax, &ebx, &ecx, &edx);
-	if(!((ebx >> 2) & 0x1)){
+	if (!((ebx >> 2) & 0x1))
+	{
 		pr_err("intel_sgx: the CPU is missing SGX\n");
 		return -ENODEV;
 	}
 
 	rdmsrl(MSR_IA32_FEAT_CTL, fc);
 
-	if (!(fc & FEAT_CTL_LOCKED)) {
+	if (!(fc & FEAT_CTL_LOCKED))
+	{
 		pr_err("intel_sgx: the feature control MSR is not locked\n");
 		return -ENODEV;
 	}
 
-	if (!(fc & FEAT_CTL_SGX_ENABLED)) {
+	if (!(fc & FEAT_CTL_SGX_ENABLED))
+	{
 		pr_err("intel_sgx: SGX is not enabled\n");
 		return -ENODEV;
 	}
 
 	cpuid(0, &eax, &ebx, &ecx, &edx);
-	if (eax < SGX_CPUID) {
+	if (eax < SGX_CPUID)
+	{
 		pr_err("intel_sgx: CPUID is missing the SGX leaf\n");
 		return -ENODEV;
 	}
 
 	cpuid_count(SGX_CPUID, SGX_CPUID_CAPABILITIES, &eax, &ebx, &ecx, &edx);
-	if (!(eax & 1)) {
+	if (!(eax & 1))
+	{
 		pr_err("intel_sgx: CPU does not support the SGX1 instructions\n");
 		return -ENODEV;
 	}
@@ -371,13 +387,14 @@ static int sgx_drv_probe(struct platform_device *pdev)
 	return sgx_dev_init(&pdev->dev);
 }
 
-static int sgx_drv_remove(struct platform_device *pdev)
+static void sgx_drv_remove(struct platform_device *pdev)
 {
 	int i;
 
-	if (!atomic_cmpxchg(&sgx_init_flag, 1, 0)) {
+	if (!atomic_cmpxchg(&sgx_init_flag, 1, 0))
+	{
 		pr_warn("intel_sgx: second release call skipped\n");
-		return 0;
+		return;
 	}
 
 	misc_deregister(sgx_device());
@@ -388,8 +405,6 @@ static int sgx_drv_remove(struct platform_device *pdev)
 		iounmap((void *)sgx_epc_banks[i].va);
 #endif
 	sgx_page_cache_teardown();
-
-	return 0;
 }
 
 #ifdef CONFIG_ACPI
@@ -404,9 +419,9 @@ static struct platform_driver sgx_drv = {
 	.probe = sgx_drv_probe,
 	.remove = sgx_drv_remove,
 	.driver = {
-		.name			= "intel_sgx",
-		.pm			= &sgx_drv_pm,
-		.acpi_match_table	= ACPI_PTR(sgx_device_ids),
+		.name = "intel_sgx",
+		.pm = &sgx_drv_pm,
+		.acpi_match_table = ACPI_PTR(sgx_device_ids),
 	},
 };
 
